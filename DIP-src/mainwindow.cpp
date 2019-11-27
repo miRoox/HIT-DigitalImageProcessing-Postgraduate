e9e1780c00efc25e55c85daeec4fe39651d95142
miRoox <miroox@outlook.com>
2019-11-27 18:20:34 +0800

    准备重构界面

--- DIP-src/mainwindow.cpp
+++ DIP-src/mainwindow.cpp
@@ -1,45 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "algorithms.h"
-#include "imageview.h"
-#include "histogramview.h"
-#include <QCompleter>
 #include <QImageReader>
-#include <QFileSystemModel>
 #include <QFileDialog>
 #include <QFileInfo>
 #include <QMessageBox>
-
-static void setupPathEdit(QLineEdit* edit)
-{
-    auto completer = new QCompleter(edit);
-    auto model = new QFileSystemModel(completer);
-    model->setRootPath("");
-    completer->setModel(model);
-    edit->setCompleter(completer);
-}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setupPathEdit(ui->originPathEdit);
-
-    connect(ui->originSelButton,&QPushButton::clicked,
-            this,&MainWindow::openImageDialog);
-    connect(ui->originPathEdit,&QLineEdit::returnPressed,
-            [this]{openImage(ui->originPathEdit->text());});
-
-    connect(ui->originView,&ImageView::imageChanged,
-            ui->originHistView,&HistogramView::calculateImageHistogram);
-
-    connect(ui->originView,&ImageView::saveImageTriggered,
-            this,&MainWindow::saveImageFromView);
-    connect(ui->globalEnhView,&ImageView::saveImageTriggered,
-            this,&MainWindow::saveImageFromView);
-    connect(ui->localEnhView,&ImageView::saveImageTriggered,
-            this,&MainWindow::saveImageFromView);
 }
 
 MainWindow::~MainWindow()
@@ -56,7 +27,7 @@ void MainWindow::openImage(const QString &fileName)
         if (image.format()!=QImage::Format_Grayscale8) {
             ui->statusbar->showMessage(tr("警告：图像已被转换为灰度格式"),10000);
         }
-        ui->originView->setImage(image.convertToFormat(QImage::Format_Grayscale8));
+//        ui->originView->setImage(image.convertToFormat(QImage::Format_Grayscale8));
     } else {
         QMessageBox::critical(this,tr("读取图像失败！"),reader.errorString(),QMessageBox::Cancel);
         ui->statusbar->showMessage(tr("读取图像失败！"),5000);
@@ -68,7 +39,6 @@ void MainWindow::openImageDialog()
     QString fileName = QFileDialog::getOpenFileName(this,tr("打开图像"),".",
                                                     tr("图片 (*.png *.jpg *.jpeg *.bmp *.xpm)"));
     if (!fileName.isEmpty()) {
-        ui->originPathEdit->setText(fileName);
         openImage(fileName);
     }
 }
