33e17b3931688c311687858fc73528307f360921
miRoox <miroox@outlook.com>
2019-12-05 17:29:58 +0800

    局部统计增强
    空实现

--- DIP-src/algorithms.h
+++ DIP-src/algorithms.h
@@ -6,5 +6,6 @@
 
 QVector<double> histogram(const QImage& image);
 QImage equalizeHistogram(const QImage& image);
+QImage localStatisticalEnhance(const QImage& image, double k0, double k1, double k2, double m);
 
 #endif // ALGORITHMS_H
